4d72caa3c8d64aced3d71a3e361704399b97b05b
Viktor Chernikov <viktor.chernikov@onet.pl>
2023-08-09 20:44:08 +0200

    Podstawowe okno

--- SystemReporter.Frontend/Main.Designer.cs
+++ SystemReporter.Frontend/Main.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FastReport = new System.Windows.Forms.Button();
+            this.NewReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FastReport
+            // 
+            this.FastReport.Location = new System.Drawing.Point(12, 12);
+            this.FastReport.Name = "FastReport";
+            this.FastReport.Size = new System.Drawing.Size(128, 37);
+            this.FastReport.TabIndex = 0;
+            this.FastReport.Text = "Szybki Raport";
+            this.FastReport.UseVisualStyleBackColor = true;
+            // 
+            // NewReport
+            // 
+            this.NewReport.Location = new System.Drawing.Point(144, 12);
+            this.NewReport.Name = "NewReport";
+            this.NewReport.Size = new System.Drawing.Size(128, 37);
+            this.NewReport.TabIndex = 1;
+            this.NewReport.Text = "Nowy Raport";
+            this.NewReport.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 61);
+            this.Controls.Add(this.NewReport);
+            this.Controls.Add(this.FastReport);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Systemowy Raporter";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button FastReport;
+        private System.Windows.Forms.Button NewReport;
     }
 }
 
